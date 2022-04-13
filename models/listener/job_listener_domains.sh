d3998e150c2fd701cf7de555b74ef4cca4376f28
dizzi <nicofirst1@gmail.com>
2022-04-13 12:24:40 +0200

    update ignore and job scheduler

--- models/listener/job_listener_domains.sh
+++ models/listener/job_listener_domains.sh
@@ -15,20 +15,29 @@ conda activate uvapb
 echo "Creating output directory..."
 mkdir "${HOME}"/pb_listener_outputs
 
+common_args="-dropout 0.5 -batch_size 32 -model_type scratch_rrr -embed_type scratch -vectors_file clip.json -reduction sum -subset_size -1 -seed 1 -learning_rate 0.0001 -shuffle"
+
 #running the actual code
 echo "Starting the process..."
 for domain in outdoor indoor vehicles food appliances
 do
-  CUDA_VISIBLE_DEVICES=0 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain -dropout 0.5 -batch_size 32 -model_type scratch_rrr -embed_type scratch -vectors_file clip.json -reduction sum -subset_size -1 -seed 1 -learning_rate 0.0001 -shuffle\
-    &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__1_{domain} &
+  CUDA_VISIBLE_DEVICES=0 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain \
+  "$common_args" \
+  &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__1_{domain} &
   sleep 60
-  CUDA_VISIBLE_DEVICES=1 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain -dropout 0.5 -batch_size 32 -model_type scratch_rrr -embed_type scratch -vectors_file clip.json -reduction sum -subset_size -1 -seed 2 -learning_rate 0.0001 -shuffle\
+
+  CUDA_VISIBLE_DEVICES=1 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain \
+      "$common_args" \
     &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__2_{domain} &
   sleep 60
-  CUDA_VISIBLE_DEVICES=2 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain -dropout 0.5 -batch_size 32 -model_type scratch_rrr -embed_type scratch -vectors_file clip.json -reduction sum -subset_size -1 -seed 3 -learning_rate 0.0001 -shuffle\
+
+  CUDA_VISIBLE_DEVICES=2 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain\
+       "$common_args" \
     &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__3_{domain} &
   sleep 60
-  CUDA_VISIBLE_DEVICES=3 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain -dropout 0.5 -batch_size 32 -model_type scratch_rrr -embed_type scratch -vectors_file clip.json -reduction sum -subset_size -1 -seed 4 -learning_rate 0.0001 -shuffle\
+
+  CUDA_VISIBLE_DEVICES=3 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain \
+        "$common_args" \
     &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__4_{domain}
   wait
 done
