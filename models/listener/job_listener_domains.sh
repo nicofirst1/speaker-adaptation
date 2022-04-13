c1157c39a50b36c01166f23b275fe71db6c4cb07
dizzi <nicofirst1@gmail.com>
2022-04-13 15:22:19 +0200

    fixed job scheduler

--- models/listener/job_listener_domains.sh
+++ models/listener/job_listener_domains.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=10:00:00
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=4
+#SBATCH --gpus-per-node=5
 
 #activating the virtual environment
 echo "Activating the virtual environment..."
@@ -19,26 +19,36 @@ common_args="-dropout 0.5 -batch_size 32 -model_type scratch_rrr -embed_type scr
 
 #running the actual code
 echo "Starting the process..."
-for domain in outdoor indoor vehicles food appliances
-do
-  CUDA_VISIBLE_DEVICES=0 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain \
+echo "Launching appliances"
+
+CUDA_VISIBLE_DEVICES=0 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain appliances \
+"$common_args" \
+&> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__1_appliances &
+sleep 60
+
+echo "Launching food"
+
+CUDA_VISIBLE_DEVICES=1 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain food \
   "$common_args" \
-  &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__1_{domain} &
-  sleep 60
+&> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__2_food &
+sleep 60
 
-  CUDA_VISIBLE_DEVICES=1 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain \
-      "$common_args" \
-    &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__2_{domain} &
-  sleep 60
+echo "Launching indoor"
 
-  CUDA_VISIBLE_DEVICES=2 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain\
-       "$common_args" \
-    &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__3_{domain} &
-  sleep 60
+CUDA_VISIBLE_DEVICES=2 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain indoor\
+   "$common_args" \
+&> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__3_indoor &
+sleep 60
+echo "Launching outdoor"
 
-  CUDA_VISIBLE_DEVICES=3 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain domain \
-        "$common_args" \
-    &> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__4_{domain}
-  wait
-done
+CUDA_VISIBLE_DEVICES=3 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain outdoor \
+    "$common_args" \
+&> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__4_outdoor
+
+echo "Launching vehicles"
+
+CUDA_VISIBLE_DEVICES=4 python -u ${HOME}/pb_speaker_adaptation/listener/train_listener_domains.py -train_domain vehicles \
+    "$common_args" \
+&> ${HOME}/pb_listener_outputs/log_listener_scratch_rrr_clip__4_vehicles
+
 
