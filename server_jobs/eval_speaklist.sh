12586526ddb37a5e21896235166fc9e941e1d28c
dizzi <nicofirst1@gmail.com>
2022-06-08 10:32:15 +0200

    fixed output redirection

--- server_jobs/eval_speaklist.sh
+++ server_jobs/eval_speaklist.sh
@@ -38,36 +38,36 @@ echo "Starting the process..."
 
 if [[ $SLURM_ARRAY_TASK_ID -eq 1 ]]; then
   echo "Launching appliances"
-  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain appliances "${common_args[@]}" > "${out_file}"
+  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain appliances "${common_args[@]}" &> "${out_file}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 2 ]]; then
 
   echo "Launching food"
 
-  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain food "${common_args[@]}" > "${out_file}"
+  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain food "${common_args[@]}" &> "${out_file}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 3 ]]; then
 
   echo "Launching indoor"
 
-  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain indoor "${common_args[@]}" > "${out_file}"
+  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain indoor "${common_args[@]}" &> "${out_file}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 4 ]]; then
 
   echo "Launching outdoor"
 
-  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain outdoor "${common_args[@]}" > "${out_file}"
+  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain outdoor "${common_args[@]}" &> "${out_file}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 5 ]]; then
 
   echo "Launching vehicles"
 
-  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain vehicles "${common_args[@]}" > "${out_file}"
+  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain vehicles "${common_args[@]}" &> "${out_file}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 6 ]]; then
   echo "Launching all"
 
-  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain all "${common_args[@]}" > "${out_file}"
+  PYTHONIOENCODING=utf-8 python -u ${trainers_file} --train_domain all "${common_args[@]}" &> "${out_file}"
 
 else
   echo "No domain specified for id $SLURM_ARRAY_TASK_ID"
