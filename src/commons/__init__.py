912791af5bc002b2eab7491fc9b8c4e52e5d24e8
dizzi <nicofirst1@gmail.com>
2022-06-22 18:50:08 +0200

    update sim check point

--- src/commons/__init__.py
+++ src/commons/__init__.py
@@ -38,10 +38,10 @@ SIM_ALL_CHK = "adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_all:v5
 
 SIM_DOMAIN_CHK = dict(
     # epoch 83
-    food="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_food:v380",
+    food="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_food:v431",
     all="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_all:v523",
-    appliances="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_appliances:v420",
-    indoor="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_indoor:v360",
-    outdoor="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_outdoor:v323",
-    vehicles="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_vehicles:v352",
+    appliances="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_appliances:v471",
+    indoor="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_indoor:v411",
+    outdoor="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_outdoor:v374",
+    vehicles="adaptive-speaker/simulator-pretrain/SimulatorModel_no_hist_vehicles:v403",
 )
