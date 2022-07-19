eeb8f887573a8b639387436ff4bd1edfb22e5511
Dizzi <nicofirst1@gmail.com>
2022-07-19 13:23:11 +0200

    fixed batch norm with drop last

--- src/trainers/simulator_pretrain.py
+++ src/trainers/simulator_pretrain.py
@@ -333,6 +333,7 @@ if __name__ == "__main__":
     load_params = {
         "batch_size": bs,
         "shuffle": shuffle,
+        "drop_last":True,
         "collate_fn": AbstractDataset.get_collate_fn(
             speaker_model.device,
             list_vocab["<sos>"],
