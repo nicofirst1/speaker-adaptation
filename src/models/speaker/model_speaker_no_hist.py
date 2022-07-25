fixed normalization

src/models/speaker/model_speaker_no_hist.py
@@ -215,7 +215,7 @@ class SpeakerModel_no_hist(nn.Module):
             torch.cat((batch_out_hidden[0], batch_out_hidden[1]), dim=1)
         )
 
-        decoder_hid=normalize(decoder_hid)
+        decoder_hid=F.normalize(decoder_hid)
 
         history_att = self.lin2att_hist(outputs)
 