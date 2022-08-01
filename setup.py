17022a388d4edd0d34a1501d41d327d01abeb2d6
dizzi <nicofirst1@gmail.com>
2022-08-01 11:31:02 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
+with open("uvapb.yml", "r") as f:
+    reqs = f.read()
+reqs= reqs.split("- pip:")[1]
+reqs= reqs.split("prefix")[0]
+reqs= reqs.split("\n")
+reqs= [x.replace("-","",1) for x in reqs if x.strip()]
+reqs= [x.strip() for x in reqs if x.strip()]
+
 setuptools.setup(
     name="PB speaker adaptation",
     version="0.1",
     packages=setuptools.find_packages(),
     long_description=long_description,
+    install_requires=reqs,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3.8",
