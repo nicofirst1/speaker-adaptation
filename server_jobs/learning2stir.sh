633ce87b06d76b91d05d1679c1a8ecc6ac023e32
dizzi <nicofirst1@gmail.com>
2022-08-04 13:30:54 +0200

    updated sh

--- server_jobs/learning2stir.sh
+++ server_jobs/learning2stir.sh
@@ -15,10 +15,9 @@ module load Anaconda3/2021.05
 source activate uvapb
 
 #create output directory
-common_args=( --dropout 0.25 --model_type no_hist --metric accs --reduction sum --subset_size -1
+common_args=( --dropout 0.25 --model_type binary --metric accs --reduction sum --subset_size -1
 --seed 42 --learning_rate 0.0001  -shuffle --embedding_dim 1024 --epochs 100 --patience 10 --s_iter 5
---learning_rate 0.01 --type_of_sim domain --pretrain_loss ce)
-
+--adapt_lr 0.3 --type_of_sim domain --pretrain_loss fbce --test_split seen --data_domain all)
 # restore the simulator
 #common_args=("${common_args[@]}" --resume_train true )
 
@@ -31,36 +30,36 @@ echo "Starting the process..."
 
 if [[ $SLURM_ARRAY_TASK_ID -eq 1 ]]; then
   echo "Launching appliances"
-  python -u ${trainers_file} --train_domain appliances "${common_args[@]}" > "${out_file}"
+  python -u ${trainers_file} --train_domain appliances "${common_args[@]}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 2 ]]; then
 
   echo "Launching food"
 
-  python -u ${trainers_file} --train_domain food "${common_args[@]}" > "${out_file}"
+  python -u ${trainers_file} --train_domain food "${common_args[@]}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 3 ]]; then
 
   echo "Launching indoor"
 
-  python -u ${trainers_file} --train_domain indoor "${common_args[@]}" > "${out_file}"
+  python -u ${trainers_file} --train_domain indoor "${common_args[@]}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 4 ]]; then
 
   echo "Launching outdoor"
 
-  python -u ${trainers_file} --train_domain outdoor "${common_args[@]}" > "${out_file}"
+  python -u ${trainers_file} --train_domain outdoor "${common_args[@]}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 5 ]]; then
 
   echo "Launching vehicles"
 
-  python -u ${trainers_file} --train_domain vehicles "${common_args[@]}" > "${out_file}"
+  python -u ${trainers_file} --train_domain vehicles "${common_args[@]}"
 
 elif [[ $SLURM_ARRAY_TASK_ID -eq 6 ]]; then
   echo "Launching all"
 
-  python -u ${trainers_file} --train_domain all "${common_args[@]}" > "${out_file}"
+  python -u ${trainers_file} --train_domain all "${common_args[@]}"
 
 else
   echo "No domain specified for id $SLURM_ARRAY_TASK_ID"
