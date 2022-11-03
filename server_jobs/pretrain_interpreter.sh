92d666110723057ba542df68d0b33d7cf5dc0484
dizzi <nicofirst1@gmail.com>
2022-11-03 16:58:34 +0100

    update pretrain

--- server_jobs/pretrain_interpreter.sh
+++ server_jobs/pretrain_interpreter.sh
@@ -18,10 +18,10 @@ source activate uvapb
 common_args=(--metric accs --reduction sum --subset_size -1  --seed 42
 -shuffle --test_split seen --data_domain all --type_of_int domain --mask_oov_embed unk --model_type tom)
 # train arguments
-common_args=("${common_args[@]}" --epochs 250 --patience 20 --pretrain_loss ce --adaptive_loss ce --learning_rate 0.001 -shuffle )
+common_args=("${common_args[@]}" --epochs 50 --patience 10 --pretrain_loss ce --adaptive_loss ce --learning_rate 0.003 -shuffle )
 
 # model arguments
-common_args=("${common_args[@]}"  --dropout 0.64  --embedding_dim 512 --hidden_dim 1024 )
+common_args=("${common_args[@]}"  --dropout 0.3  --embedding_dim 128 --hidden_dim 128 --attention_dim 128 )
 # restore the interpreter
 #common_args=("${common_args[@]}" --resume_train true )
 
